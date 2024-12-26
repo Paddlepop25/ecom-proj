6e30cb791dd56348fb12c3d3bd9f6ce6ac6e1fab
Paddlepop25 <shecodeshsucodes18@gmail.com>
2024-12-26 20:47:58 +0800

    search for products

--- src/main/java/com/telusko/ecom_proj/controller/ProductController.java
+++ src/main/java/com/telusko/ecom_proj/controller/ProductController.java
@@ -103,4 +103,11 @@ public class ProductController {
             return new ResponseEntity<>("Cannot delete, product not found", HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword) {
+        System.out.println(">>> searching with --> " + keyword);
+        List<Product> products = service.searchProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
--- src/main/java/com/telusko/ecom_proj/repository/ProductRepository.java
+++ src/main/java/com/telusko/ecom_proj/repository/ProductRepository.java
@@ -2,9 +2,20 @@ package com.telusko.ecom_proj.repository;
 
 import com.telusko.ecom_proj.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 // JpaRepository<ClassWorkingWith, PrimaryKeyType (would be id type)>
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    // this is not SQL query.
+    // this is JPQL query
+    @Query("SELECT p from Product p WHERE "+"LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR "
+            +"LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR "
+            +"LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR "
+            +"LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
 }
